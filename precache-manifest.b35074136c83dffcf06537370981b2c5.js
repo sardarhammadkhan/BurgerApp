a4fce3115071967e7e3053d02092ea4895045e24
Hammad <hammad@ahmed.com>
2020-09-16 20:04:02 +0500

    Updates

---
+++ precache-manifest.b35074136c83dffcf06537370981b2c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4881f4a36af50a0bfadf044755b33b07",
+    "url": "/BurgerApp/index.html"
+  },
+  {
+    "revision": "ebbaef1648f659efb616",
+    "url": "/BurgerApp/static/css/2.cd85e1b1.chunk.css"
+  },
+  {
+    "revision": "b8b6d18fc81524ff08f7",
+    "url": "/BurgerApp/static/css/main.77273bf2.chunk.css"
+  },
+  {
+    "revision": "ebbaef1648f659efb616",
+    "url": "/BurgerApp/static/js/2.0b10fab4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BurgerApp/static/js/2.0b10fab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b6d18fc81524ff08f7",
+    "url": "/BurgerApp/static/js/main.68c170cb.chunk.js"
+  },
+  {
+    "revision": "13c91809c23aee572e0c",
+    "url": "/BurgerApp/static/js/runtime-main.13150ef2.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerApp/static/media/burgerlogo.b8503d26.png"
+  }
+]);
